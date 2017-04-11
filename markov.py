64b7723df1c5319cd4a2c695f31e8fb96621e275
Hackbright Student <no-reply@hackbrightacademy.com>
2017-04-11 12:25:58 -0700

    Markov Chains - 1st Code Review

--- markov.py
+++ markov.py
@@ -5,19 +5,26 @@ from random import choice
 
 
 def open_and_read_file(file_path):
-    """Takes file path as string; returns text as string.
+    """
+    
+    Takes file path as string; returns text as string.
 
-    Takes a string that is a file path, opens the file, and turns
-    the file's contents as one string of text.
+    Takes a string that is a file path, opens the file, and 
+    Turns the file's contents as one string of text.
+    
     """
 
     # your code goes here
+    # read the file, return as text as a string titled "contents"
+    contents = open(file_path).read()
 
-    return "Contents of your file as one long string"
-
+    # return "Contents of your file as one long string"
+    return contents
 
 def make_chains(text_string):
-    """Takes input text as string; returns dictionary of markov chains.
+    """
+    
+    Takes input text as string; returns dictionary of markov chains.
 
     A chain will be a key that consists of a tuple of (word1, word2)
     and the value would be a list of the word(s) that follow those two
@@ -30,19 +37,41 @@ def make_chains(text_string):
     Each bigram (except the last) will be a key in chains:
 
         >>> sorted(chains.keys())
-        [('hi', 'there'), ('mary', 'hi'), ('there', 'mary')]
+        [('hi', 'there'), ('mary', 'hi'), ('there', 'juanita'), ('there', 'mary')]
 
     Each item in chains is a list of all possible following words:
 
         >>> chains[('hi', 'there')]
         ['mary', 'juanita']
+
     """
 
+    # Make a list of words in text_string
+    words = text_string.split()
+
+    # create dictionary
     chains = {}
 
-    # your code goes here
+    # Iterate over the index numbers of the list
+    for i in range(len(words)-2):
+        
+        # create a tuple of two n-grams
+        bigrams = (words[i], words[i+1])
+
+        # check for repeat of keys / bigrams
+        if bigrams not in chains:
+
+            # if the key doesn't exists, add key to chains
+            chains[bigrams] = [words[i+2]]
+
+       # if bigram is in the list, append value to the type list
+        else:
+            chains[bigrams].append(words[i+2])
+
 
     return chains
+
+    # Code has been revieweed and works up to this point
 
 
 def make_text(chains):
@@ -54,7 +83,6 @@ def make_text(chains):
 
     return " ".join(words)
 
-
 input_path = "green-eggs.txt"
 
 # Open the file and turn it into one long string
@@ -63,7 +91,9 @@ input_text = open_and_read_file(input_path)
 # Get a Markov chain
 chains = make_chains(input_text)
 
-# Produce random text
-random_text = make_text(chains)
+print chains
 
-print random_text
+# # Produce random text
+# random_text = make_text(chains)
+
+# print random_text
